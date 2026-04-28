10d87847b90944bfd9aff68a3b7c4f3a21c8991e
Krutarth Pipaliya <krutarth.pipaliya@simformsolutions.com>
2026-04-28 11:57:21 +0530

    refact: add try catch for parsing

--- src/eventHandlers/addHistory.ts
+++ src/eventHandlers/addHistory.ts
@@ -3,26 +3,34 @@ import type { History } from "../types.js";
 
 export default function addHistory(str = "") {
     const localHistory = localStorage.getItem("calculation-history");
-    let history = localHistory
-        ? (JSON.parse(localHistory) as Array<History>)
-        : [];
-    if (history) history.unshift([displayString, str]);
-    localStorage.setItem("calculation-history", JSON.stringify(history));
-    renderHistory();
+    try {
+        let history = localHistory
+            ? (JSON.parse(localHistory) as Array<History>)
+            : [];
+        if (history) history.unshift([displayString, str]);
+        localStorage.setItem("calculation-history", JSON.stringify(history));
+        renderHistory();
+    } catch (error) {
+        if (error instanceof Error) console.log("error", error);
+    }
 }
 
 export function renderHistory() {
     const localHistory = localStorage.getItem("calculation-history");
-    let history = localHistory
-        ? (JSON.parse(localHistory) as Array<History>)
-        : ([] as Array<History>);
+    try {
+        let history = localHistory
+            ? (JSON.parse(localHistory) as Array<History>)
+            : ([] as Array<History>);
 
-    let ul = document.querySelector(".history-list");
-    if (ul) ul.innerHTML = "";
-    for (let [expression, result] of history) {
-        const li = document.createElement("li");
-        li.className = "history-item";
-        li.textContent = `${expression} = ${result}`;
-        ul?.appendChild(li);
+        let ul = document.querySelector(".history-list");
+        if (ul) ul.innerHTML = "";
+        for (let [expression, result] of history) {
+            const li = document.createElement("li");
+            li.className = "history-item";
+            li.textContent = `${expression} = ${result}`;
+            ul?.appendChild(li);
+        }
+    } catch (error) {
+        if (error instanceof Error) console.log("error", error);
     }
 }
