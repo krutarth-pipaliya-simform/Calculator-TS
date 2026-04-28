ac3d401701598a9e1406ef79a8623538234815ce
Krutarth Pipaliya <krutarth.pipaliya@simformsolutions.com>
2026-04-28 11:38:32 +0530

    refact: add type check for button and div in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import buttonClick from "./eventHandlers/buttonClick.js";
 import toggleHistory from "./eventHandlers/toggleHistory.js";
 
 const btn = document.querySelector(".main-header-history");
-if (btn) {
+if (btn instanceof HTMLButtonElement) {
     btn.addEventListener("click", toggleHistory);
 }
 
 const operations = document.querySelector(".buttons-grid-wrapper");
-if (operations) {
+if (operations instanceof HTMLDivElement) {
     operations.addEventListener("click", buttonClick);
 }
